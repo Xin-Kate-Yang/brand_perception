8853bbb0f441c46d4e196937df1fcae1d267db29
Xin Yang (Kate) <34717817+Xin-Kate-Yang@users.noreply.github.com>
2025-06-18 13:29:18 -0700

    Update IAT_main.js
    change image folder url to io

--- IAT_main.js
+++ IAT_main.js
@@ -51,7 +51,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
 		},	
 
 		base_url : {//Where are your images at?
-			image : 'https://github.com/Xin-Kate-Yang/brand_perception/tree/main/images/'
+			image : 'https://xin-kate-yang.github.io/brand_perception/images/'
 		} 
 	});
 });
